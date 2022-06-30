490f169dac1f77a836f2b199f889cb421a0b4ada
Eduard B <edd.bba@gmail.com>
2022-06-30 22:39:15 +0100

    finished the recipe card styling

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -7,21 +7,21 @@ const RecipeCard = ({ recipe }) => {
   return (
     <div id="recipe-card">
       <div id="recipe-img-container">
-        <img src={recipe.images.SMALL.url} />
+        <img alt="recipe" src={recipe.images.SMALL.url} />
       </div>
       <div id="recipe-text-container">
         <h1>{recipe.label}</h1>
         <div id="recipe-text-info">
           <div className="recipe-text">
-            <h2>Calories</h2>
+            <h4>Calories</h4>
             <p>{Math.floor(recipe.calories)}</p>
           </div>
           <div className="recipe-text">
-            <h2>Portions</h2>
+            <h4>Portions</h4>
             <p>{recipe.yield}</p>
           </div>
           <div className="recipe-text">
-            <h2>Ingredients</h2>
+            <h4>Ingredients</h4>
             <p>{recipe.ingredients.length}</p>
           </div>
         </div>
